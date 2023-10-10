6d58c79b3050ee5c4c07512b616417525690675b
Mcx002 <muchlishchoeruddin96@gmail.com>
2023-10-10 09:08:06 +0700

    feat(anonymous): Add create anonymous customer token

---
+++ src/servers/anonymous.ts
@@ -0,0 +1,13 @@
+import {sendUnaryData, ServerUnaryCall} from "@grpc/grpc-js";
+import {createCustomerAnonymousToken} from "../services/anonymous";
+import {AnonymousDto, AuthResultDto} from "../../proto_gen/auth_pb";
+
+export const createAnonymousCustomerTokenServer = async (call: ServerUnaryCall<AnonymousDto, AuthResultDto>, callback: sendUnaryData<AuthResultDto>) => {
+    const req = call.request
+    const token = await createCustomerAnonymousToken(req.getUsername(), req.getPassword())
+
+    const authResultDto = new AuthResultDto()
+    authResultDto.setToken(token)
+
+    callback(null, authResultDto)
+}
