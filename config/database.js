e8b9b07afc5def917b5136a22ba05235d7c26004
Mcx002 <muchlishchoeruddin96@gmail.com>
2023-10-10 09:04:09 +0700

    Add database configuration

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        host: process.env.DB_HOSTNAME,
+        host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: process.env.DB_DIALECT,
         dialectOptions: {
@@ -27,7 +27,7 @@ module.exports = {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        host: process.env.DB_HOSTNAME,
+        host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: process.env.DB_DIALECT,
         dialectOptions: {
--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,23 @@
 import {getEnvNumber, getEnvString} from "./utils/env-parser";
 
-export const config = {
+export const appConfig = {
     // Server Configuration
     port: getEnvNumber("PORT", 40000),
     serviceName: getEnvString("SERVICE_NAME", "AuthService"),
     serviceVersion: getEnvString("SERVICE_VERSION", "v0.0.1"),
 
     // Jwt
-    jwtSecretKey: getEnvString("JWT_PRIVATE_KEY", 'secretKey'),
-    jwtIssuer: getEnvString("JWT_ISSUER", 'loginservice.mcstore.com'),
+    jwtSecretKey: getEnvString("JWT_PRIVATE_KEY"),
+    jwtIssuer: getEnvString("JWT_ISSUER", "loginservice.mcstore.com"),
 
     // Expired Times
     anonymousTokenExpiredTime: getEnvNumber("ANONYMOUS_TOKEN_EXPIRED_TIME", 86400),
+
+    // Database
+    dbDialect: getEnvString("DB_DIALECT", "postgres"),
+    dbUsername: getEnvString("DB_USERNAME", "postgres"),
+    dbPassword: getEnvString("DB_PASSWORD"),
+    dbName: getEnvString("DB_NAME", "mcstore-auth"),
+    dbHost: getEnvString("DB_HOST", "localhost"),
+    dbPort: getEnvNumber("DB_PORT", 5432),
 }
