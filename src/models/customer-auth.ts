357121c9324fdfe34a02313b4a40b7ea86a3f4ff
Mcx002 <muchlishchoeruddin96@gmail.com>
2023-10-15 10:16:48 +0700

    Add customer auth model

---
+++ src/models/customer-auth.ts
@@ -0,0 +1,60 @@
+import {DataTypes, Model, Optional, Sequelize} from "sequelize";
+import {BaseAttributes} from "./index";
+
+export interface CustomerAuthAttributes extends BaseAttributes {
+    id: number
+    userId: number
+    email: string
+    password: string
+}
+
+export type CustomerAuthCreationAttributes = Optional<CustomerAuthAttributes, "id">
+
+export class CustomerAuth extends Model implements CustomerAuthAttributes {
+    createdAt!: Date
+    email!: string
+    password!: string
+    id!: number
+    updatedAt!: Date
+    userId!: number
+    version!: number
+
+    static initModel(sequelize: Sequelize): void {
+        CustomerAuth.init({
+            id: {
+                type: DataTypes.BIGINT,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            userId: {
+                type: DataTypes.BIGINT,
+                allowNull: false,
+                unique: true,
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            createdAt: {
+                type: DataTypes.DATE,
+                allowNull: false
+            },
+            updatedAt: {
+                type: DataTypes.DATE,
+                allowNull: false
+            },
+            version: {
+                type: DataTypes.SMALLINT,
+                allowNull: false
+            }
+        }, {
+            sequelize,
+            tableName: 'CustomerAuth'
+        })
+    }
+}
