bb327c98d9ac5a330950ef8fca94c4002421deb4
Mcx002 <muchlishchoeruddin96@gmail.com>
2023-10-17 17:00:58 +0700

    Add validate customer account service

--- src/services/customer.ts
+++ src/services/customer.ts
@@ -5,6 +5,7 @@ import {Status} from "@grpc/grpc-js/build/src/constants";
 import {createBaseAttributes} from "../models";
 import {findCustomerByEmail, insertCustomerAuth} from "../repositories/customer";
 import {getUnixFromDate} from "../utils/time";
+import { createHash } from "crypto";
 
 export const registerCustomerAuth = async (payload: CustomerAuthDto) => {
     const password = payload.getPassword()
@@ -40,4 +41,24 @@ export const composeCustomerAuthDto = (payload: CustomerAuthAttributes): Custome
     result.setUserId(payload.userId)
 
     return result
+}
+
+export const validateCustomerAccount = async (payload: CustomerAuthDto): Promise<boolean> => {
+    const customerAuth = await findCustomerByEmail(payload.getEmail())
+    if (!customerAuth) {
+        throw new ErrorHandler(Status.INVALID_ARGUMENT, "customer auth not found")
+    }
+
+    const password = payload.getPassword()
+    if (!password) {
+        throw new ErrorHandler(Status.INVALID_ARGUMENT, "password required")
+    }
+
+    const hashedPassword = createHash('sha256').update(password).digest('hex')
+
+    if (customerAuth.password !== hashedPassword) {
+        throw new ErrorHandler(Status.INVALID_ARGUMENT, "auth invalid")
+    }
+
+    return true
 }
--- tests/unit/services/customer.test.ts
+++ tests/unit/services/customer.test.ts
@@ -1,6 +1,7 @@
 import {CustomerAuthDto} from "../../../proto_gen/customer-auth_pb";
-import {isCustomerEmailExists, registerCustomerAuth} from "../../../src/services/customer";
+import {isCustomerEmailExists, registerCustomerAuth, validateCustomerAccount} from "../../../src/services/customer";
 import {CustomerAuthAttributes} from "../../../src/models/customer-auth";
+import {createHash} from 'crypto';
 
 describe('Service registerCustomerAuth Test', () => {
     test('Should throw Password required', async () => {
@@ -39,5 +40,47 @@ describe('Service registerCustomerAuth Test', () => {
 
         const isEmailExists = await isCustomerEmailExists('test@email.com')
         expect(isEmailExists).toBe(false)
+    })
+
+})
+
+describe('Service validateCustomerAccount Test', () => {
+    test('Should throw customer auth not found error', async () => {
+        const customerRep = require('../../../src/repositories/customer')
+        jest.spyOn(customerRep, 'findCustomerByEmail').mockReturnValue(null)
+
+        expect(async () => validateCustomerAccount(new CustomerAuthDto())).rejects.toThrow("customer auth not found")
+    })
+
+    test('Should throw password required', async () => {
+        const customerRep = require('../../../src/repositories/customer')
+        jest.spyOn(customerRep, 'findCustomerByEmail').mockReturnValue({})
+
+        expect(async () => validateCustomerAccount(new CustomerAuthDto())).rejects.toThrow("password required")
+    })
+
+    test('Should throw auth invalid', async () => {
+        const customerRep = require('../../../src/repositories/customer')
+        jest.spyOn(customerRep, 'findCustomerByEmail').mockReturnValue({})
+
+        const customerAuthDto = new CustomerAuthDto()
+        customerAuthDto.setPassword('test')
+
+        expect(async () => validateCustomerAccount(customerAuthDto)).rejects.toThrow("auth invalid")
+    })
+
+    test('Should return true', async () => {
+        const password = 'test'
+        const hashedPassword = createHash('sha256').update(password).digest('hex')
+        const customerRep = require('../../../src/repositories/customer')
+        jest.spyOn(customerRep, 'findCustomerByEmail').mockReturnValue({
+            password: hashedPassword
+        })
+
+        const customerAuthDto = new CustomerAuthDto()
+        customerAuthDto.setPassword(password)
+
+        const result = await validateCustomerAccount(customerAuthDto)
+        expect(result).toBe(true)
     })
 })
